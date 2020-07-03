de6291b5dda7e6f7c156814b11d36d5b3c559903
Cassio Rangel Fernandes <cassio.rfernandes@gmail.com>
2020-07-03 00:58:50 -0300

    Validador tela login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  profileForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]]
+  });
+
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+  get email() {
+    return this.profileForm.get('email');
+  }
+  get senha() {
+    return this.profileForm.get('senha');
+  }
+
+  public hasError = (controlName: string, errorName: string) =>{
+    return this.profileForm.controls[controlName].hasError(errorName);
+  }
+
+  onSubmit() {
+    console.log(this.profileForm.value)
   }
 
 }
